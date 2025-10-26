3eef56ebf3f9f5ec4fa5ac496d5374a50fed3fda
Bhavika1195 <bhavikasmart15@gmail.com>
2025-10-26 21:25:17 +0530

    credentials_added

--- server/index.js
+++ server/index.js
@@ -13,15 +13,8 @@ dotenv.config();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.log(err));
-
-  app.use('/api/products', productRoutes);
+app.use('/api/products', productRoutes);
 app.use('/api/payment', paymentRoutes);
-
 
 connectDB();
 
